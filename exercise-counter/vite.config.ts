1f1aa0122f1e1c113edba829b012ecf15eb329d5
arnav2711 <arnav2007mody@gmail.com>
2025-07-23 16:09:48 +0530

    E and C changed

--- exercise-counter/vite.config.ts
+++ exercise-counter/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace YOUR-REPO-NAME below with your actual repo name
 export default defineConfig({
-  plugins: [react()],
+  base: '/Exercise-Counter/', // <-- this must match repo name
+  plugins: [react()]
 })
